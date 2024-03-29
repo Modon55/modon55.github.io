2dc28b3deb973cfad92d9a0f0e1feb85c0d46638
Modon55 <68068809+Modon55@users.noreply.github.com>
2024-03-29 01:46:24 +0100

    Version 1.5 (alpha state)
    CHANGES:
    + Fixed issue when images cannot be saved to gallery
    + Fixed issue(s) with browser history when saving images
    
    NOTE: Maintenance work has been carried out to avoid serious problems with your browser. Everything is now clear!

---
+++ lang_es.js
@@ -0,0 +1,13 @@
+// spanish
+
+const languageData = {
+    toolsHeader: "Herramientas",
+    galleryHeader: "Su galería personal",
+    galleryHeader2: "Carga los gráficos de píxeles guardados.",
+    saveBTN: "Guardar",
+    galleryBTN: "Galería",
+    gridsizeLabel: "Tamaño rejilla: ",
+    gallerywarningTXT: "Las imágenes guardadas se guardan localmente. Si se borra el historial del navegador, la galería se vacía por completo.",
+    gridsizewarningTXT: "Dibujo actual restablece cuando se cambia tamaño",
+    galleryemptyTXT: "Por desgracia, aquí no hay nada que ver :("
+};
